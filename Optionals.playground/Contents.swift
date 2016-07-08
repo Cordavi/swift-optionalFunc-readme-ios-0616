ee5079ab274b0049e843775a638ef9b1f8698faa
Cordavi <michaelamundsen@gmail.com>
2016-07-08 09:51:37 -0400

    Completed code-along by implementing provided code to see how optional functions work

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -1,15 +1,8 @@
-func brokenFirstItem(items: [String]) -> String {
-    return items[0]
-}
+//func firstItem(items: [String]) -> String {
+//    return items[0]
+//}
 
-var fruits = ["apple", "banana", "grape"]
-brokenFirstItem(fruits)  // Works!
-fruits = []
-// Uncomment the next line to see the problem.
-// You will get an "index out of range" error.
-//brokenFirstItem(fruits)  // Breaks :-(
-
-
+var fruits: [String] = []
 
 func firstItem(items: [String]) -> String? {
     if items.isEmpty {
@@ -18,24 +11,24 @@ func firstItem(items: [String]) -> String? {
     return items[0]
 }
 
-fruits = ["apple", "banana", "grape"]
 firstItem(fruits)
 
+
+fruits = ["apple", "banana", "grape"]
 if let fruit = firstItem(fruits) {
     print("The first fruit is \(fruit)")
 } else {
     print("There are no fruits!")
 }
+// Prints "The first fruit is apple"
 
 fruits = []
-firstItem(fruits)
-
 if let fruit = firstItem(fruits) {
     print("The first fruit is \(fruit)")
 } else {
     print("There are no fruits!")
 }
-
+// Prints "There are no fruits!"
 
 func produceFullName(withFirstName firstName: String, middleName: String?, lastName: String) -> String {
     var name = firstName
@@ -47,4 +40,7 @@ func produceFullName(withFirstName firstName: String, middleName: String?, lastN
 }
 
 produceFullName(withFirstName: "Jon", middleName: "Bon", lastName: "Jovi")
+// Returns "Jon Bon Jovi"
+
 produceFullName(withFirstName: "Mary", middleName: nil, lastName: "Doe")
+// Returns "Mary Doe"
